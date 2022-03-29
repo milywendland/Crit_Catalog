be76e4c3829081d9347d93167016b3a5f1577130
milywendland <milynicwendland@gmail.com>
2022-03-28 19:45:37 -0600

    fixed problem in app.js

--- client/src/pages/Games.js
+++ client/src/pages/Games.js
@@ -15,32 +15,31 @@ const Games = () => {
     }
     getGames()
   }, [])
+
+  let navigate = useNavigate()
+
+  const showGame = (gameId) => {
+    navigate(`/game/details/${gameId}`)
+  }
+
+  return (
+    <div>
+      <h2>All Games</h2>
+      <section className="container">
+        <div className="games-wrapper">
+          {games.map((game) => (
+            <div key={game._id}>
+              <GameCard
+                name={game.name}
+                image={game.image}
+                description={game.description}
+                onClick={() => showGame(game._id)}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  )
 }
-
-let navigate = useNavigate()
-
-const showGame = (gameId) => {
-  navigate(`/game/details/${gameId}`)
-}
-
-return (
-  <div>
-    <h2>All Games</h2>
-    <section className="container">
-      <div className="games-wrapper">
-        {games.map((game) => (
-          <div key={game._id}>
-            <GameCard
-              name={game.name}
-              image={game.image}
-              description={game.description}
-              onClick={() => showGame(game._id)}
-            />
-          </div>
-        ))}
-      </div>
-    </section>
-  </div>
-)
-
 export default Games
