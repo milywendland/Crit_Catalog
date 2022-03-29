c9f374f569e083106193128aa0bca08b124c248b
milywendland <milynicwendland@gmail.com>
2022-03-29 17:50:31 -0600

    added button to gamecards

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -1,6 +1,7 @@
 const GameCard = (props) => {
   return (
     <div className="card" onClick={props.onClick}>
+      <button>+</button>
       <h3>{props.name}</h3>
       <img src={props.image} alt={props.name}></img>
     </div>
